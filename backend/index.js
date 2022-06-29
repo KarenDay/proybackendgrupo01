213f98f91e6b63daa81cf18fe2b81afc5fc897d1
Ayala <eduflo2@hotmail.com>
2022-06-29 20:30:20 -0300

    Rol,Anuncio,Medio,Index,Database

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const {mongoose} = require('./database');
+var app = express();
+
+
+//middlewares
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+
+
+//Cargamos el modulo de direccionamiento de rutas
+app.use('/api/rol', require('./routers/rol.route'));
+app.use('/api/area', require('./routers/area.route'));
+app.use('/api/persona', require('./routers/persona.route'));
+app.use('/api/medio', require('./routers/medio.route'));
+app.use('/api/anuncio', require('./routers/anuncio.route'));
+
+
+//setting
+app.set('port', process.env.PORT || 3000);
+
+//starting the server
+app.listen(app.get('port'), () => {
+console.log(`Server started on port`, app.get('port'));
+});
